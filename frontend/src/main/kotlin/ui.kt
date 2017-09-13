5243d427840af71ba8b0f1e604228aef39e472e0
Jan Michael Auer <mail@jauer.org>
2017-09-13 18:05:36 +0200

    Styling

--- frontend/src/main/kotlin/ui.kt
+++ frontend/src/main/kotlin/ui.kt
@@ -1,34 +1,43 @@
+
 import domain.Todo
 import kotlinx.html.*
 import kotlinx.html.js.onChangeFunction
+import kotlinx.html.js.onClickFunction
 import org.w3c.dom.HTMLInputElement
 
-fun <T, C : kotlinx.html.TagConsumer<T>> C.Todo(todo: Todo) = label {
-    checkBoxInput {
+fun <T, C : kotlinx.html.TagConsumer<T>> C.Todo(todo: Todo) = div {
+    onClickFunction = { toggleTodo(todo) }
+
+    checkBoxInput(classes = "toggle") {
         checked = todo.completed
-        onChangeFunction = { toggleTodo(todo) }
     }
 
-    +todo.title
+    label {
+        +todo.title
+    }
 }
 
 fun <T, C : kotlinx.html.TagConsumer<T>> C.Todos(todos: List<Todo>) = div {
     id = "todos"
 
-    h1 { +"TODO Application" }
-    ul {
-        todos.forEach { todo ->
-            li {
-                Todo(todo)
+    h1 { +"Todos" }
+
+    header(classes = "header") {
+        textInput(classes = "new-todo") {
+            placeholder = "What would you like TODO?"
+            onChangeFunction = { event ->
+                val input = event.target!! as HTMLInputElement
+                createTodo(input.value)
+                input.value = ""
             }
         }
     }
 
-    textInput {
-        onChangeFunction = { event ->
-            val input = event.target!! as HTMLInputElement
-            createTodo(input.value)
-            input.value = ""
+    ul(classes = "todo-list") {
+        todos.forEach { todo ->
+            li {
+                Todo(todo)
+            }
         }
     }
 }
