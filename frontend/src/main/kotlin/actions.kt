8af020edae1bb46f2a80142a48849a3b48e06419
Jan Michael Auer <mail@jauer.org>
2017-09-13 17:46:59 +0200

    Use lodash.uniqueId

--- frontend/src/main/kotlin/actions.kt
+++ frontend/src/main/kotlin/actions.kt
@@ -1,7 +1,6 @@
 import domain.Todo
 import org.w3c.fetch.RequestInit
 import kotlin.browser.window
-import kotlin.js.Math
 
 private fun putTodo(todo: Todo) {
     launch {
@@ -15,7 +14,7 @@ private fun putTodo(todo: Todo) {
 }
 
 fun createTodo(title: String) {
-    val id = Math.random().toString()
+    val id = Lodash.uniqueId("todo_")
     val todo = Todo(id = id, title = title)
     putTodo(todo)
 }
---
+++ frontend/src/main/kotlin/lodash.kt
@@ -0,0 +1,4 @@
+@JsModule("lodash")
+external object Lodash {
+    fun uniqueId(text: String = definedExternally): String;
+}
