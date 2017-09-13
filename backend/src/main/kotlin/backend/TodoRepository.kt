49512218e2b6d2328f075ed0e13feb39abefc8ea
Martin Prebio <martin.prebio@gmail.com>
2017-09-13 17:44:29 +0200

    Back TodoStore by List instead of Map

--- backend/src/main/kotlin/backend/TodoRepository.kt
+++ backend/src/main/kotlin/backend/TodoRepository.kt
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.*
-import kotlin.collections.HashMap
+import kotlin.collections.ArrayList
 
 @Repository
 class TodoRepository {
-    private val store = Collections.synchronizedMap(HashMap<String, Todo>())
+    private val store = Collections.synchronizedList(ArrayList<Todo>())
 
     init {
         add(Todo(id = randomUUID(), title = "Bootstrap Project", completed = true))
@@ -19,7 +19,7 @@ class TodoRepository {
 
     private final fun randomUUID() = UUID.randomUUID().toString()
 
-    final fun getAll(): Flux<Todo> = Flux.fromIterable(store.values)
-    final fun get(id: String): Mono<Todo> = Mono.justOrEmpty(store[id])
-    final fun add(todo: Todo) = store.put(todo.id, todo)
+    final fun getAll(): Flux<Todo> = Flux.fromStream(store.stream())
+    final fun get(id: String): Mono<Todo> = Mono.justOrEmpty(store.firstOrNull { id == it.id })
+    final fun add(todo: Todo) = store.add(todo)
 }
