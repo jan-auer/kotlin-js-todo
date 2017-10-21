387fba900149a850e48d586b300d46aa6cdd617d
Martin Prebio <martin.prebio@gmail.com>
2017-10-21 14:50:46 +0200

    Fix compile-time warning

--- frontend/src/main/kotlin/coroutines.kt
+++ frontend/src/main/kotlin/coroutines.kt
@@ -18,8 +18,8 @@ fun launch(block: suspend () -> Unit) {
     block.startCoroutine(object : Continuation<Unit> {
         override val context: CoroutineContext get() = EmptyCoroutineContext
         override fun resume(value: Unit) {}
-        override fun resumeWithException(e: Throwable) {
-            console.log("Coroutine failed: $e")
+        override fun resumeWithException(exception: Throwable) {
+            console.log("Coroutine failed: $exception")
         }
     })
 }
